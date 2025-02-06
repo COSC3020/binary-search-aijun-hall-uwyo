817c96dd5358de225844e261c78c135c0d0ce265
aijun-hall-uwyo <aijun.hall@uwyo.edu>
2025-02-05 17:44:26 -0700

    merge conflict resolve tryagain

--- code.js
+++ code.js
@@ -1,6 +1,5 @@
 function binarySearch(list, element) {
     // Assuming list is already sorted because of >given a sorted list from README
-
     let starting_index = 0;
     let ending_index = list.length - 1;
 
@@ -10,7 +9,8 @@ function binarySearch(list, element) {
 
         if (list[middle_index] === element) {
             return middle_index;
-            
+        }
+
         if (list[middle_index] < element) {
             starting_index = middle_index + 1;  // Shift binary search to the right, since element is larger than middle
         } else {
