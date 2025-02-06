e67ecbe5dc98f88ccf63b9c6a0352e3b28f73498
aijun-hall-uwyo <aijun.hall@uwyo.edu>
2025-02-05 17:43:37 -0700

    merge conflict wierdness from initial feedback commit

--- code.js
+++ code.js
@@ -10,17 +10,7 @@ function binarySearch(list, element) {
 
         if (list[middle_index] === element) {
             return middle_index;
-
-    let starting_index = 1;
-    let ending_index = list.length;
-
-    while (starting_index <= ending_index) {
-        let middle_index = (starting_index + ending_index) / 2;
-
-        if (list[middle_index] == element) {
-            return true;
-        }
-
+            
         if (list[middle_index] < element) {
             starting_index = middle_index + 1;  // Shift binary search to the right, since element is larger than middle
         } else {
