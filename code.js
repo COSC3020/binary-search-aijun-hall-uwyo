ab82ee33872dbc8e97b1b478df0c37f95f3163a7
aijun-hall-uwyo <aijun.hall@uwyo.edu>
2025-02-05 17:27:09 -0700

    push directly to main, list.length - 1 fix

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 function binarySearch(list, element) {
+    // Assuming list is already sorted because of >given a sorted list from README
+    let starting_index = 1;
+    let ending_index = list.length - 1;
+
+    while (starting_index <= ending_index) {
+        let middle_index = (starting_index + ending_index) / 2;
+
+        if (list[middle_index] == element) {
+            return true;
+        }
+
+        if (list[middle_index] < element) {
+            starting_index = middle_index + 1;  // Shift binary search to the right, since element is larger than middle
+        } else {
+            ending_index = middle_index - 1; // Shift binary search to the left, since element is less than the middle
+        }
+    }
+
     return -1;
 }
